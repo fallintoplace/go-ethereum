76d2b897180292d37fa69f1be869a8ecbba5edb3
jwasinger <j-wasinger@hotmail.com>
2026-05-13 12:43:42 -0400

    Apply suggestion from @MariusVanDerWijden
    Co-authored-by: Marius van der Wijden <m.vanderwijden@live.de>

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -148,9 +148,6 @@ func NewEVM(blockCtx BlockContext, statedb StateDB, chainConfig *params.ChainCon
 		jumpDests:   newMapJumpDests(),
 		arena:       newArena(),
 	}
-	if !evm.chainRules.IsAmsterdam {
-		fmt.Println("DEBUG")
-	}
 	evm.precompiles = activePrecompiledContracts(evm.chainRules)
 
 	switch {
