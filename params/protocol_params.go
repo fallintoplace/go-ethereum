2373db618afd81c3d08ceaef8abfc02ed63d77d5
Gary Rong <garyrong0905@gmail.com>
2026-05-18 15:37:16 +0800

    core/state, params: update params and remove SD state refund

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -198,10 +198,10 @@ const (
 	// the bound has a small safety margin for system-contract accesses that
 	// don't consume block gas.
 	BALItemCost               uint64 = 2000
-	AccountCreationSize              = 112
-	StorageCreationSize              = 32
+	AccountCreationSize              = 120
+	StorageCreationSize              = 64
 	AuthorizationCreationSize        = 23
-	CostPerStateByte                 = 1174
+	CostPerStateByte                 = 1530
 )
 
 // Bls12381G1MultiExpDiscountTable is the gas discount table for BLS12-381 G1 multi exponentiation operation
